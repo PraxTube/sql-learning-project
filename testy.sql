ba72f9e24717a80075ef31e515ceb09710a15c34
PraxTube <lukinator28@gmail.com>
2024-05-22 10:04:13 +0200

    restructure!: Rename files to be less ambigious

--- testy.sql
+++
@@ -1,9 +0,0 @@
-create sequence termin_id start 1;
-CREATE TABLE my_table (
-    ID uinteger primary key default nextval('termin_id'),
-    column1 TEXT NOT NULL DEFAULT 'default text',
-    column2 INTEGER NOT NULL DEFAULT 100,
-    column3 TEXT
-);
-
-insert into my_table (id, column3) values (0, 'hi mom');
