afead7cd4fbcabc5441f048acdb39724a461797d
PraxTube <lukinator28@gmail.com>
2024-05-22 09:48:44 +0200

    feat: Add more of the constrains (though not passing more tests)

--- main.sql
+++ main.sql
@@ -18,38 +18,42 @@ CREATE TABLE Patient_in (
 
 -- CORRECT
 CREATE TABLE Diagnose (
-    ICD varchar PRIMARY KEY,
+    ICD varchar PRIMARY KEY check (length(ICD) == 3 and left(ICD, 1) ~ '^[A-Z]' and right(ICD, 2) ~ '^[0-9]{9}$'),
     Zusatzinformation CHAR NOT NULL,
     Beschreibung varchar,
 );
 
 create sequence termin_id start 1;
 create table Termin (
-    ID uinteger primary key default nextval('termin_id'),
+    ID uinteger primary key default nextval('termin_id') check (ID <= 3_000_000),
     Zeitpunkt datetime not null,
     Zusatzgebühren usmallint not null check (Zusatzgebühren <= 500) default 0,
     ist_Neupatient_in boolean not null,
     Ärzt_in varchar not null,
-    Patient_in varchar not null,
     foreign key(Ärzt_in) references Ärzt_in(LANR),
-    foreign key(Patient_in) references Patient_in(Versichertennummer),
+    unique (Ärzt_in, Zeitpunkt),
 );
 
 CREATE TABLE Gesundheitseinrichtung (
-    SteuerID varchar not null,
+    SteuerID varchar not null check (length(SteuerID) == 11 and starts_with(SteuerID, 'DE') and right(SteuerID, 9) ~ '^[0-9]{9}$'),
     Name varchar not null,
-    Bundesland varchar not null,
+    Bundesland varchar not null check (length(Bundesland) == 5 and starts_with(Bundesland, 'DE-')),
     Adresse varchar not null,
-    Umsatz int not null,
+    Umsatz decimal(15, 2) not null,
+    Typ varchar not null check (Typ == 'Krankenhaus' or Typ == 'Privatpraxis'),
     primary key (SteuerID, Name),
+);
 
+create table Krankenhaus (
     Betten INTEGER,
     Bettenauslastung FLOAT,
     ist_privatisiert BOOLEAN,
     ist_Universitätsklinikum BOOLEAN,
+);
 
+create table Privatpraxis (
     Fachrichtung varchar,
-    Zahlungsart varchar,
+    Zahlungsart varchar check (Zahlungsart == 'Versichert' or Zahlungsart == 'Selbstzahler'),
 );
 
 -- CORRECT
@@ -62,8 +66,8 @@ create table "OP-Saal" (
 );
 
 CREATE TABLE OP (
-    Nummer INTEGER PRIMARY KEY,
-    Dringlichkeit ubigint not null,
+    Nummer ubigint PRIMARY KEY,
+    Dringlichkeit varchar not null check (Dringlichkeit == 'Notoperation' or Dringlichkeit == 'dringliche Operation' or Dringlichkeit == 'frühelektive Operation' or Dringlichkeit == 'elektive Operation'),
     ist_Vollnarkose boolean not null,
     Datum date not null,
     Startzeit time not null,
@@ -71,9 +75,10 @@ CREATE TABLE OP (
     Patient_in varchar not null,
     SteuerID varchar not null,
     Name varchar not null,
-    Raumnummer utinyint not null,
+    "OP-Saal" utinyint not null,
     foreign key(Patient_in) references Patient_in(Versichertennummer),
-    foreign key(SteuerID, Name, Raumnummer) references "OP-Saal"(SteuerID, Name, Raumnummer),
+    foreign key(SteuerID, Name, "OP-Saal") references "OP-Saal"(SteuerID, Name, Raumnummer),
+    check (datesub('minute', Endzeit, Startzeit) >= 15 and datesub('minute', Endzeit, Startzeit) <= 9 * 60)
 );
 
 --
@@ -87,6 +92,7 @@ create table stellt (
     primary key (Ärzt_in, Diagnose),
     foreign key(Ärzt_in) references Ärzt_in(LANR),
     foreign key(Diagnose) references Diagnose(ICD),
+    unique (Ärzt_in, Zeitpunkt),
 );
 
 create table angestellt (
@@ -97,4 +103,14 @@ create table angestellt (
     Name varchar not null,
     foreign key(Ärzt_in) references Ärzt_in(LANR),
     foreign key(SteuerID, Name) references Gesundheitseinrichtung(SteuerID, Name),
+);
+
+create table hat (
+    Termin uinteger not null,
+    Diagnose varchar not null,
+    Patient_in varchar not null,
+    primary key (Termin, Diagnose),
+    foreign key (Termin) references Termin(ID),
+    foreign key (Diagnose) references Diagnose(ICD),
+    foreign key (Patient_in) references Patient_in(Versichertennummer),
 );
